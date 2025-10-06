5c6b791c4ba30c7e748f717855c412bf126b93ad
Ömer Halis Demir <153090813+omerhd34@users.noreply.github.com>
2025-10-07 01:10:47 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',  
-  images: {
-    unoptimized: true,
-  },
-  swcMinify: false,
-  experimental: {
-    workerThreads: false,
-    cpus: 1
-  }
 }
 export default nextConfig;
